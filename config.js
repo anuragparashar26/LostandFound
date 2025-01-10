597a52b4e5fcd14eef4259ca0d882286a6851883
Anurag Parashar <111885714+anuragparashar26@users.noreply.github.com>
2025-01-10 22:50:20 +0530

    updated

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-};
+    apiKey: "AIzaSyAxrbSJocAUES-WeIlQYp-rfSqxFIZ3vy0",
+    authDomain: "lostandfound-21453.firebaseapp.com",
+    databaseURL: "https://lostandfound-21453-default-rtdb.firebaseio.com",
+    projectId: "lostandfound-21453",
+    storageBucket: "lostandfound-21453.firebasestorage.app",
+    messagingSenderId: "39047409772",
+    appId: "1:39047409772:web:94db7bdd22d9ccfe06e849"
+  };
 
 export { firebaseConfig };
