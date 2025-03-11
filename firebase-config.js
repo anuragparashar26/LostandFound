1e7cedd83c9d86b243668d54ada7f1fcdc85155d
anuragparashar26 <anuragparashar5025@gmail.com>
2025-03-11 18:48:56 +0530

    updated

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/13.33.0/firebase-app.js";
 async function loadFirebaseConfig() {
   try {
       const response = await fetch('/.netlify/functions/firebase-config');
