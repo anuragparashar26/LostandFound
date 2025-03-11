83c7d31e92d5f84986ddef1c050eb73e9571c0dc
anuragparashar26 <anuragparashar5025@gmail.com>
2025-03-11 18:30:43 +0530

    updated apis

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from "../firebase/firebase-config.js";
+import { firebaseConfig } from "../firebase-config.js";
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const signoutBtn = document.querySelector('#signoutbtn');
